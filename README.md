b06553152c1bf83a7dc4567635b932d4dc9a0805
mslxl <i@mslxl.com>
2023-09-17 23:06:11 +0800

    chore: 初使化 nix 环境

---
+++ README.md
@@ -0,0 +1,3 @@
+# Provlegisto
+
+WIP...
---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  description = "Tauri Javascript App";
+
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, fenix }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlay ];
+        };
+        toolchain = pkgs.fenix.complete;
+        buildInputs = with pkgs; [
+          # js
+          nodejs
+          nodePackages.pnpm
+
+          # rust
+          (with toolchain; [
+            cargo
+            rustc
+            rust-src
+            clippy
+            rustfmt
+          ])
+          pkg-config
+          openssl
+
+          # tauri
+          webkitgtk
+          dbus
+        ];
+      in
+      rec {
+
+        # Executed by `nix build`
+        packages.default = pkgs.mkYarnPackage rec {
+          inherit buildInputs;
+          name = "template";
+          src = ./.;
+
+          buildPhase = "yarn build";
+
+          installPhase = ''
+            mkdir -p $out/bin
+
+            cp src-tauri/target/release/${name} $out/bin/${name}
+          '';
+        };
+
+        # Executed by `nix run`
+        apps.default = utils.lib.mkApp {
+          drv = packages.default;
+        };
+
+        # Used by `nix develop`
+        devShell = pkgs.mkShell {
+          inherit buildInputs;
+
+          # Specify the rust-src path (many editors rely on this)
+          RUST_SRC_PATH = "${toolchain.rust-src}/lib/rustlib/src/rust/library";
+        };
+
+      });
+}

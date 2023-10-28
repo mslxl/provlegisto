6bfd7b857164987a2dcb3a2a1b6e076245a31315
mslxl <i@mslxl.com>
2023-10-28 22:37:49 +0800

    fix(pref): 修复配置项无法写入本地文件
    Co-authored-by: xia0ne <1223605525@qq.com>

--- src/lib/presistedSettings.ts
+++ src/lib/presistedSettings.ts
@@ -3,11 +3,11 @@ import { useSettingStore } from "../store/settings"
 import { invoke, window } from "@tauri-apps/api"
 import { map } from "ramda"
 
-async function getPresistItem(name: string): Promise<string | null> {
-  return await invoke<string | null>("get_presist_settings", { name })
+async function getPresistItem(): Promise<string | null> {
+  return await invoke<string | null>("get_presist_settings", { name: "settings" })
 }
-async function setPresistItem(name: string, value: string): Promise<void> {
-  await invoke("set_presist_settings", { name, value })
+export async function setPresistItem(value: string): Promise<void> {
+  await invoke("set_presist_settings", { name: "settings", value })
 }
 
 // Reference: https://github.com/prazdevs/pinia-plugin-persistedstate/issues/214#issuecomment-1605923720
@@ -19,19 +19,19 @@ export async function createPresistedSettings(): Promise<Pinia> {
 
     await Promise.all(
       map(async (store): Promise<void> => {
-        const data = await getPresistItem(store.$id)
+        const data = await getPresistItem()
         console.log(data)
         if (data !== null) {
           store.$patch(JSON.parse(data))
         } else {
-          setPresistItem(store.$id, JSON.stringify(store.$state)).catch(console.error)
+          setPresistItem(JSON.stringify(store.$state)).catch(console.error)
         }
 
         /// 仅由主窗口写配置
         /// 其他窗口应通过向主窗口同步状态更新配置
         if (window.getCurrent().label === "main") {
           store.$subscribe(() => {
-            setPresistItem(store.$id, JSON.stringify(store.$state)).catch(console.error)
+            setPresistItem(JSON.stringify(store.$state)).catch(console.error)
           })
         }
       }, stores),
--- src/lib/syncPref.ts
+++ src/lib/syncPref.ts
@@ -1,14 +1,12 @@
-import { invoke } from "@tauri-apps/api"
 import bus from "../bus"
 import { type SettingsState } from "../store/settings"
+import { setPresistItem } from "./presistedSettings"
 export async function syncPreferenceCrossWindows(state: SettingsState): Promise<void> {
   await Promise.all([bus.emitCrossWindows("sync:pref", state), syncPreferenceToLocal(state)])
 }
 
 export async function syncPreferenceToLocal(state: SettingsState): Promise<void> {
-  await invoke("sync_settings", {
-    data: JSON.stringify(state),
-  })
+  await setPresistItem(JSON.stringify(state))
 }
 
 export function useListenPreferenceChangeHook(dispatcher: (state: SettingsState) => void): void {

204541161737e993926840d4b781ddaecf548cf0
mslxl <i@mslxl.com>
2023-11-17 22:37:02 +0800

    build(npm): 添加相关依赖库

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css"
-
-function App() {
-  return <div className="container"></div>
-}
-
-export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import path from "path"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -13,5 +19,5 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
-  }
-}));
+  },
+}))
